547fad42b780f3cc73ef1ebf51f9ad11358d6b06
MoonSzol <96268163+MoonSzol@users.noreply.github.com>
2023-05-28 10:25:35 -0600

    Create ex3.cpp

---
+++ ex3.cpp
@@ -0,0 +1,28 @@
+/*
+ * Programing w/ Mosh - ex: 3
+ * Jesus M Avila V
+ * 5/28/23
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    double sales = 95000;
+    const double stateTaxRate = 0.04;
+    const double countyTaxRate = 0.02;
+
+    double stateTax = sales * stateTaxRate;
+    double countyTax = sales * countyTaxRate;
+    double totalTax = stateTax + countyTax;
+    double totalIncome = sales - totalTax;
+
+    cout << "Sales = $" << sales << endl
+              << "State tax = $" << stateTax << endl
+              << "County tax = $" << countyTax << endl
+              << "Total Tax = $" << totalTax << endl
+              << "Sales after tax = $" << totalIncome;
+    return 0;
+}
