61c59beec881d9b22c81ad335b9a9bfd8db0e868
MoonSzol <96268163+MoonSzol@users.noreply.github.com>
2023-05-28 14:22:28 -0600

    Update Ex6 28May2023

--- Ex6.cpp
+++ Ex6.cpp
@@ -11,14 +11,6 @@ using namespace std;
 
 int main()
 {
-    double fahrenheit;
-    double fahToCelsius;
 
-    cout << "input a temperature in degrees Fahrenheit: ";
-    cin >> fahrenheit;
-
-    fahToCelsius = (fahrenheit - 32) / 1.8;
-
-    cout << fahrenheit << "F is " << fahToCelsius << "C";
     return 0;
 }
